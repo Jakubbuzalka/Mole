9f9c2acbce4f46034720b962f3f555d242d438dc
Miacik <cacmiac@gmail.com>
2020-05-10 13:22:52 +0200

    Added mol class to another file + some ew calculations

---
+++ Mol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc
+{
+    class Mol
+    {
+        private double avgConst = 6.022 * Math.Pow(10, 23);
+        public double weight;
+
+        public double WeightOfOne()
+        {
+            double result;
+            result = weight / avgConst;
+
+            return result;
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,29 +4,38 @@ namespace Calc
 {
     class Program
     {
-       
-        class Mol
-        {
-            private double avgConst = 6.022 * Math.Pow(10, 23);
-            public double weight;
-
-            public double WeightOfOne()
-            {
-                double result;
-                result = weight / avgConst;
-
-                return result;
-            }
-
-        }
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi human gib weight.");
-            Mol mol = new Mol();
-            mol.weight = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Weight of one is :"+"\n"+ Convert.ToString(mol.WeightOfOne()));
+            bool ui = false;
+
+            if (ui)
+            {
+                Console.WriteLine("Hi human gib weight.");
+                Mol mol = new Mol();
+                mol.weight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Weight of one is :"+"\n"+ Convert.ToString(mol.WeightOfOne()));
+
+            }
+
+            Mol carbon = new Mol();
+            Mol oxygen = new Mol();
+            Mol nitro = new Mol();
+            Mol hydro = new Mol();
+            Mol sulfur = new Mol();
+
+            carbon.weight = 12.0107;
+            oxygen.weight = 15.9994;
+            nitro.weight = 14.0067;
+            hydro.weight = 1.00794;
+            sulfur.weight = 32.065;
+
+            // c3 h7 no2 s
+
+            double meta = carbon.WeightOfOne() * 3 + hydro.WeightOfOne() * 7 + oxygen.WeightOfOne() * 2 + nitro.WeightOfOne() + sulfur.WeightOfOne();
+            double result = 0.000126 / meta - 1;
+
+            Console.WriteLine(result);
+
         }
 
         
